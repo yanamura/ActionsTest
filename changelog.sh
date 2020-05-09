7db7c34bdfbe4e8af6377bdb99ccfeed7088ae6e
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-09 15:34:56 +0900

    fix quote

--- changelog.sh
+++ changelog.sh
@@ -6,4 +6,4 @@ changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
 changelog="${changelog//$'\r'/'%0D'}"
 
-echo "::set-output name=changelog::$changelog
+echo "::set-output name=changelog::$changelog"
