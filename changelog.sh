cb30de7a498fe6597fe6fd2ab64a22349f40dfea
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-09 15:30:27 +0900

    fix

---
+++ changelog.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+changelog = $(git diff @~2)
+
+changelog="${changelog//'%'/'%25'}"
+changelog="${changelog//$'\n'/'%0A'}"
+changelog="${changelog//$'\r'/'%0D'}"
+
+echo "::set-output name=changelog::$changelog
