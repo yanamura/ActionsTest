317d047c7df2fedaa8ebc174b12d3837e99503d7
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-09 23:53:22 +0900

    debug

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo $(git tag --sort version:refname | tail -n 2)
+echo $(git tag --sort version:refname | tail -n 2 | head -n 1)
+echo $(git tag --sort version:refname | tail -n 1)
+echo $(git log --merges --pretty=format:"* %b)
+
 git log $(git tag --sort version:refname | tail -n 2 | head -n 1)..$(git tag --sort version:refname | tail -n 1) --merges --reverse --pretty=format:"* %b"
 echo $(git log $(git tag --sort version:refname | tail -n 2 | head -n 1)..$(git tag --sort version:refname | tail -n 1) --merges --reverse --pretty=format:"* %b")
 
@@ -9,5 +12,7 @@ changelog=$(git log $(git tag --sort version:refname | tail -n 2 | head -n 1)..$
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
 changelog="${changelog//$'\r'/'%0D'}"
+
+echo $changelog
 
 echo "::set-output name=changelog::$changelog"
