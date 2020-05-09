a43df51a8b18fe5f01052968581495284809fa8b
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-09 17:54:02 +0900

    #hoge changelog

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-changelog=$(git diff @~2)
+changelog=$(git log $(git tag --sort version:refname | tail -n 2 | head -n 1)..$(git tag --sort version:refname | tail -n 1) --merges --reverse --pretty=format:"* %b")
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
