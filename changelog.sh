4b62290d60978d24a1199e5ae749cee7f0927835
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-09 23:40:27 +0900

    add debug

--- changelog.sh
+++ changelog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo $(git tag --sort version:refname | tail -n 2)
+echo $(git log $(git tag --sort version:refname | tail -n 1)..$1 --merges --reverse --pretty=format:"* %b")
+
 changelog=$(git log $(git tag --sort version:refname | tail -n 1)..$1 --merges --reverse --pretty=format:"* %b")
 
 changelog="${changelog//'%'/'%25'}"
