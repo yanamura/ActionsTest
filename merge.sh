eddf12b95c37babd7df0ae1e8e999242630b7ee2
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-17 15:01:33 +0900

    merge action

---
+++ merge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RELEASE_BRANCH=$1
+
+git config --global user.name "yanamura"
+git config --global user.email "yanamura3@gmail.com"
+
+# merge to master
+git checkout master
+git pull --rebase origin master
+git merge ${RELEASE_BRANCH} --no-ff --no-edit
+git push origin master
+
+# add tag
+VERSION=${RELEASE_BRANCH#release-*}
+git tag ${VERSION}
+git push origin --tags
+
+# merge to develop
+git checkout develop
+git pull --rebase origin develop
+git merge ${RELEASE_BRANCH} --no-ff --no-edit
+git push origin develop
